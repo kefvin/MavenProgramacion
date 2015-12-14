280444e6665f817ee76761815c3496028aa8540c
alumne1daw <alumne1daw@localhost>
2015-12-14 19:09:42 +0100

    Metodo CPapa (count papa) creado

--- MavenProgramacion/src/main/java/MavenProgramacion/MavenProgramacion/App.java
+++ MavenProgramacion/src/main/java/MavenProgramacion/MavenProgramacion/App.java
@@ -12,31 +12,54 @@ import java.io.Reader;
  * Hello world!
  *
  */
-public class App 
-{
+public class App{
+	
+	public static String casa;
+	public static final String papa = "*<]:-DOo";
+	public static int Cpapa;
+	public static final String reno = ">:o)";
+	public static int Creno;
+	public static final String elfo = "<]:-D";
+	public static int Celfo;
+	
     public static void main( String[] args ) throws IOException
     {
     	//FileReader fr = new FileReader("/home/alumne1daw/git/MavenProgramacion/MavenProgramacion/src/main/resources/MonAscii.txt");
+    	App app = new App();
     	
-    	
-    	
-    	String casa;
-    	String papa = "*<]:-DOo";
-    	String reno = ">:o)";
-    	String elfo = "<]:-D";
+    	BufferedReader br = new BufferedReader(new InputStreamReader(
+    			App.class.getResource("/MonAscii.txt").openStream()));
     	
     	try{
-    		BufferedReader br = new BufferedReader(new InputStreamReader(
-        			App.class.getResource("/MonAscii.txt").openStream()));
-    		
     		while((casa = br.readLine()) != null) { 
     			/* Llamar funciones como comprovarPapa 
     			 * y apartir del primer caràcter de los personajes
     			 * crear substring de la misma longitud y comparar*/
+    			app.comprovarPapa();
     		}
     	}catch(Exception e){
-    		
+    		System.out.println(e);
+    	}finally{
+    		br.close();
     	}
     	
     }
+    
+	public void comprovarPapa(){
+		Cpapa=0;
+		String comprovaPapa;
+		char primerPapa = papa.charAt(0);
+		
+		for(int i=0; i<casa.length(); i++){
+			if(casa.charAt(i) == primerPapa){
+				comprovaPapa = casa.substring(casa.charAt(i), (casa.charAt(i)+papa.length()-1));
+				if(comprovaPapa.equals(papa)){
+					Cpapa++;
+				}
+			}
+			
+		}
+		
+	}
 }
+
