b5fbb23f007e3d204cf47546a91d9422bfe0ba1f
alumne1daw <alumne1daw@localhost>
2015-12-15 17:45:25 +0100

    Metodos de reno y elfo creados, no va nada

--- MavenProgramacion/src/main/java/MavenProgramacion/MavenProgramacion/App.java
+++ MavenProgramacion/src/main/java/MavenProgramacion/MavenProgramacion/App.java
@@ -14,6 +14,7 @@ import java.io.Reader;
  */
 public class App{
 	
+	public static int Ncasa = 0;
 	public static String casa;
 	public static final String papa = "*<]:-DOo";
 	public static int Cpapa;
@@ -32,10 +33,20 @@ public class App{
     	
     	try{
     		while((casa = br.readLine()) != null) { 
+    			System.out.println("0");
+    			Ncasa++;
     			/* Llamar funciones como comprovarPapa 
     			 * y apartir del primer caràcter de los personajes
     			 * crear substring de la misma longitud y comparar*/
+    			
+    			System.out.println("Casa "+Ncasa);
+    			
     			app.comprovarPapa();
+    			System.out.println("1");
+    			app.comprovarReno();
+    			System.out.println("2");
+    			app.comprovarElfo();
+    			System.out.println("3");
     		}
     	}catch(Exception e){
     		System.out.println(e);
@@ -46,20 +57,57 @@ public class App{
     }
     
 	public void comprovarPapa(){
+		
 		Cpapa=0;
 		String comprovaPapa;
 		char primerPapa = papa.charAt(0);
 		
 		for(int i=0; i<casa.length(); i++){
+			
 			if(casa.charAt(i) == primerPapa){
+				
 				comprovaPapa = casa.substring(casa.charAt(i), (casa.charAt(i)+papa.length()-1));
 				if(comprovaPapa.equals(papa)){
+					
 					Cpapa++;
+					
 				}
 			}
-			
 		}
-		
+		System.out.print("Papa Noel "+Cpapa);
 	}
+	
+	public void comprovarReno(){
+		Creno=0;
+		String comprovaReno;
+		char primerReno = reno.charAt(0);
+		
+		for(int i=0; i<casa.length(); i++){
+			if(casa.charAt(i) == primerReno){
+				comprovaReno = casa.substring(casa.charAt(i), (casa.charAt(i)+reno.length()-1));
+				if(comprovaReno.equals(reno)){
+					Creno++;
+				}
+			}
+		}
+		System.out.print("Reno "+Creno);
+	}
+	
+	public void comprovarElfo(){
+		Celfo=0;
+		String comprovaElfo;
+		char primerElfo = elfo.charAt(0);
+		
+		for(int i=0; i<casa.length(); i++){
+			if(casa.charAt(i) == primerElfo){
+				comprovaElfo = casa.substring(casa.charAt(i), (casa.charAt(i)+elfo.length()-1));
+				if(comprovaElfo.equals(elfo)){
+					Celfo++;
+				}
+			}
+		}
+		System.out.print("Elfo "+Celfo);
+	}
+	
 }
 
